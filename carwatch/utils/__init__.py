697fe6cb9233061a791c61f2f2382562abffc5fb
a-mosquito <annika-muecke@outlook.de>
2023-01-15 16:54:41 +0100

    add qr generator to cli

--- carwatch/utils/__init__.py
+++ carwatch/utils/__init__.py
@@ -1,2 +1,3 @@
 from carwatch.utils.utils import assert_is_dir, assert_file_ending, write_to_file, tex_to_pdf, sanitize_str_for_tex
 from carwatch.utils.study import Study
+from carwatch.utils.click_helper import Condition, validate_subject_path, validate_mail_input, validate_saliva_distances
